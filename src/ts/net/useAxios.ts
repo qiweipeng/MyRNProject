103b4c777954573bb6fc1e05e62497d4c6b5f301
Weipeng Qi <qiweipeng@hotmail.com>
2022-08-19 20:31:10 +0800

    Update useAxios.ts

--- src/ts/net/useAxios.ts
+++ src/ts/net/useAxios.ts
@@ -77,12 +77,13 @@ const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
     Reducer<State<T, D, R>, Action<T, D, R>>
   >(reducer, initialState);
 
-  const source = useMemo(() => axios.CancelToken.source(), []);
+  const source = useMemo(() => axios.CancelToken.source(), []); // 网络请求可取消
   const instance = useMemo(
     () => axios.create({cancelToken: source.token}),
     [source],
-  );
+  ); // 创建实例，保证每个网络请求互补影响
 
+  // 使用 useRef 保存 config 参数，可使 config 的变更不会触发副作用而导致再次请求；同时，如果 config 变更后再次请求，也能使用到变更后的 config 参数
   const configRef = useRef(config);
   useEffect(() => {
     configRef.current = config;
@@ -92,6 +93,11 @@ const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
     async (c?: AxiosRequestConfig<D>) => {
       dispatch({type: 'start'});
       try {
+        // config 的覆盖规则
+        // 1. 调用时的 config 增量覆盖初始 config
+        // 2. 对于 params 和 data，调用时按字段增量覆盖初始的设置
+        //    比如初始参数为 {name: 'Jack'}，调用时如果写 {age: 18}，则最终为 {name: 'Jack', age: 18}
+        //    比如初始参数为 {name: 'Jack'}，调用时如果写 {name: 'Rose', age: 18}，则最终为 {name: 'Rose', age: 18}
         const r = await instance.request<T, R, D>({
           ...configRef.current,
           ...c,
@@ -114,7 +120,7 @@ const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
     [instance],
   );
   const fetchData = useCallback(
-    (c?: AxiosRequestConfig) => {
+    (c?: AxiosRequestConfig<D>) => {
       fetchDataAsync(c).catch(() => {
         return;
       });
