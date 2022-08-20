8598e180a0dbd0a5ee86476295f699bc4472fe04
Weipeng Qi <qiweipeng@hotmail.com>
2022-08-20 23:22:56 +0800

    useAxios 增加 loading 延迟功能

--- src/ts/net/useAxios.ts
+++ src/ts/net/useAxios.ts
@@ -61,6 +61,7 @@ const initialState = {
 
 type Options = {
   manual?: boolean; // default is true
+  loadingDelay?: number; // default is 0
 };
 
 const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
@@ -77,13 +78,12 @@ const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
     Reducer<State<T, D, R>, Action<T, D, R>>
   >(reducer, initialState);
 
-  const source = useMemo(() => axios.CancelToken.source(), []); // 网络请求可取消
+  const source = useMemo(() => axios.CancelToken.source(), []);
   const instance = useMemo(
     () => axios.create({cancelToken: source.token}),
     [source],
-  ); // 创建实例，保证每个网络请求互补影响
+  );
 
-  // 使用 useRef 保存 config 参数，可使 config 的变更不会触发副作用而导致再次请求；同时，如果 config 变更后再次请求，也能使用到变更后的 config 参数
   const configRef = useRef(config);
   useEffect(() => {
     configRef.current = config;
@@ -91,13 +91,10 @@ const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
 
   const fetchDataAsync = useCallback(
     async (c?: AxiosRequestConfig<D>) => {
-      dispatch({type: 'start'});
+      const timeout = setTimeout(() => {
+        dispatch({type: 'start'});
+      }, options?.loadingDelay ?? 0);
       try {
-        // config 的覆盖规则
-        // 1. 调用时的 config 增量覆盖初始 config
-        // 2. 对于 params 和 data，调用时按字段增量覆盖初始的设置
-        //    比如初始参数为 {name: 'Jack'}，调用时如果写 {age: 18}，则最终为 {name: 'Jack', age: 18}
-        //    比如初始参数为 {name: 'Jack'}，调用时如果写 {name: 'Rose', age: 18}，则最终为 {name: 'Rose', age: 18}
         const r = await instance.request<T, R, D>({
           ...configRef.current,
           ...c,
@@ -107,9 +104,11 @@ const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
               ? {...configRef.current.data, ...c.data}
               : configRef.current.data,
         });
+        clearTimeout(timeout);
         dispatch({type: 'resolve', response: r});
         return r;
       } catch (e) {
+        clearTimeout(timeout);
         dispatch({
           type: 'reject',
           error: e as AxiosError<unknown, D> | Error | Cancel,
@@ -117,7 +116,7 @@ const useAxios = <T = unknown, D = unknown, R = AxiosResponse<T, D>>(
         return Promise.reject(e);
       }
     },
-    [instance],
+    [instance, options?.loadingDelay],
   );
   const fetchData = useCallback(
     (c?: AxiosRequestConfig<D>) => {
