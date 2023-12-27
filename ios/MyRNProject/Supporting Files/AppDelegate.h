853a5018c992152c93f593ac01bc9e95eaa40e59
Weipeng Qi <qiweipeng@hotmail.com>
2023-12-27 16:03:22 +0800

    安装依赖包

---
+++ ios/MyRNProject/Supporting Files/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  MyRNProject
+//
+//  Created by Weipeng Qi on 2023/12/27.
+//
+
+#import <UIKit/UIKit.h>
+#import <RCTAppDelegate.h>
+
+@interface AppDelegate : RCTAppDelegate
+
+@end
