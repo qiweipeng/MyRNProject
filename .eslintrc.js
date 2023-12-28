af1245340da5f25a3b1cbbf57b73729f9f06a083
Weipeng Qi <qiweipeng@hotmail.com>
2023-12-28 11:18:10 +0800

    配置 ESLint 和绝对路径

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      plugins: ['@typescript-eslint'],
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
 };
