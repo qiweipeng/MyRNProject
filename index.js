526b00782df8799f254584775910031cda4fd221
Weipeng Qi <qiweipeng@hotmail.com>
2024-02-01 14:48:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+
+LogBox.ignoreLogs([
+  // React Navigation 导致的警告
+  'Sending `onAnimatedValueUpdate` with no listeners registered.',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
